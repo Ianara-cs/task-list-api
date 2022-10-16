949e36cf9219ce36c9aab60a22031938ce53c887
Ianara-cs <ianarasilva333@gmail.com>
2022-10-16 17:29:29 -0300

    .

--- build/server.js
+++ build/server.js
@@ -3,10 +3,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.printEnv = void 0;
+require("dotenv/config");
 const express_1 = __importDefault(require("express"));
 require("express-async-errors");
 require("reflect-metadata");
-const config_1 = require("./config/config");
 const AppError_1 = require("./errors/AppError");
 const routes_1 = require("./routes");
 require("./shared/container");
@@ -16,6 +17,10 @@ app.use(routes_1.routes);
 app.get("/", (req, res) => {
     res.status(200).json({ messege: "OK!" });
 });
+const printEnv = (value) => {
+    return value;
+};
+exports.printEnv = printEnv;
 app.use((err, request, response, next) => {
     if (err instanceof AppError_1.AppError) {
         return response.status(err.statusCode).json({
@@ -27,6 +32,6 @@ app.use((err, request, response, next) => {
         message: `Internal server error - ${err.message}`
     });
 });
-app.listen(config_1.PORT, () => {
+app.listen((0, exports.printEnv)(process.env.PORT), () => {
     console.log("Servidor rodando!🛸");
 });
--- build/util/jwt.js
+++ build/util/jwt.js
@@ -5,14 +5,14 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.verifyToken = exports.generateToken = void 0;
 const jsonwebtoken_1 = __importDefault(require("jsonwebtoken"));
-const auth_1 = __importDefault(require("../config/auth"));
+const server_1 = require("../server");
 function generateToken(user) {
-    return jsonwebtoken_1.default.sign({ userId: user.id, email: user.email }, auth_1.default.jwt_access_secret, {
-        expiresIn: auth_1.default.expires_in_token,
+    return jsonwebtoken_1.default.sign({ userId: user.id, email: user.email }, (0, server_1.printEnv)(process.env.JWT_ACCESS_SECRET), {
+        expiresIn: (0, server_1.printEnv)(process.env.EXPIRES_IN_TOKEN),
     });
 }
 exports.generateToken = generateToken;
 function verifyToken(token) {
-    return jsonwebtoken_1.default.verify(token, auth_1.default.jwt_access_secret);
+    return jsonwebtoken_1.default.verify(token, (0, server_1.printEnv)(process.env.JWT_ACCESS_SECRET));
 }
 exports.verifyToken = verifyToken;
